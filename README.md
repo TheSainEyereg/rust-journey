78b4ea4f5d0716fa2a8097fcba110d008588a25e
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2025-02-08 13:04:12 +0300

    Ownership

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This repo will contain my journey through the Rust programming language.
 | 📅 | 🚧 | ✅ | 💀 |
 
 1. [✅](/base) Basic language featurees like loops, variables, functions
-0. [📅](/memory) Lifetimes and memory management
+0. [🚧](/memory) Lifetimes and memory management
 0. [📅](/project) Structs and project architecture
 0. [📅](/multithreading) Multithreading with "shared variables"
 0. [📅](/fs) FS access, fs watch, file streams.
--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,3 +1,59 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 5;
+    let y = plus_one(n);
+    println!("The value of y is: {y}");
+
+    let a = Box::new([0 as usize; 1_000]);
+    let b = a;
+
+    println!("{}", b[10]);
+
+    let name = String::from("Ferris");
+
+    let mut name = add_suffix_borrow(name);
+    add_suffix_ref(&mut name);
+
+    let new_name = add_suffix_borrow(name.clone());
+
+    println!("{name} and {new_name}");
+
+    let lifetime = return_string_static();
+    let lifetime_my = return_string_my();
+    println!("{lifetime} {lifetime_my}");
+
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    let num: &mut i32 = &mut v[2];
+    // We can't do v[1] = 5; since some element was borrowed by num
+    *num += 1;
+    println!("Third element is {}", *num);
+    println!("Vector is now {:?}", v);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn add_suffix_borrow(mut name: String) -> String {
+    name.push_str("🚀");
+    name
+}
+
+fn add_suffix_ref(name: &mut String) {
+    add_sufix_ref_really(name); // References are not borrowed, so we'll still have access to them
+    println!("Appended suffix to string \"{name}\"");
+}
+
+fn add_sufix_ref_really(name: &mut String) {
+    name.push_str("🚀");
+}
+
+/// Wtf is lifetime
+fn return_string_static() -> &'static str {
+    let a: &str = "Hello";
+    a
+}
+
+fn return_string_my<'a>() -> &'a str {
+    let a: &str = "Hello";
+    a
 }
